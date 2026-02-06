44bdcc8cae93f94fc35fb2305f1e6de9b86704d7
BryamCode <bryamfernandezuriarte22@gmail.com>
2026-02-06 17:09:25 -0500

    Creacion del servicio de productos

---
+++ src/main/java/com/example/Stocknet_product_service/StocknetProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Stocknet_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocknetProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocknetProductServiceApplication.class, args);
+	}
+
+}
